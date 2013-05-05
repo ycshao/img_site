cff1320ec09d2ecfe1ebece36c27f7dcc202705a
ycshao <ycshao0402@gmail.com>
2013-05-04 22:22:35 -0400

    display title and picFile in admin page

--- img_site/img_uploader/admin.py
+++ img_site/img_uploader/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from img_site.img_uploader.models import PictureFile
 
-admin.site.register(PictureFile)
+class PictureFileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'picFile')
+
+admin.site.register(PictureFile, PictureFileAdmin)
