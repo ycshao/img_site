eccbff8f0647890f1245bc002f292c62d96f11d0
ycshao <ycshao0402@gmail.com>
2013-05-12 00:01:01 -0400

    add static files

--- img_site/settings.py
+++ img_site/settings.py
@@ -46,7 +46,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__),"media")
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,10 +71,9 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/static/admin/', '/usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin/',
 )
 
-ADMIN_STATIC_ROOT = '/usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/admin/'
+ADMIN_STATIC_ROOT = '/static/admin/'
 
 # List of finder classes that know how to find static files in
 # various locations.
--- img_site/urls.py
+++ img_site/urls.py
@@ -17,7 +17,6 @@ urlpatterns = patterns('',
 	
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.ADMIN_STATIC_ROOT}),
 )
 
 urlpatterns += patterns('img_site.views',
