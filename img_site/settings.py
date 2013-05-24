e6a142677dd2bc23b051798750ea2eb8c0970ac5
lnhote <lnhote@gmail.com>
2013-05-25 06:43:40 +0800

    use separate root and url path for static files.

--- img_site/settings.py
+++ img_site/settings.py
@@ -10,10 +10,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+CURRENT_PATH = os.path.dirname(__file__)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(__file__), 'database/sqlite.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(CURRENT_PATH, 'database/sqlite.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -46,31 +47,31 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__),"media")
-
+MEDIA_ROOT = os.path.join(CURRENT_PATH,"media")
+IMG_UPLOAD_PATH = os.path.join(MEDIA_ROOT, 'upload/')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
-
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+IMG_UPLOAD_URL = os.path.join(MEDIA_URL, 'upload/')
+#ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(CURRENT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	#(CSS_URL, CSS_PATH),
 )
 
 # List of finder classes that know how to find static files in
@@ -132,7 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 	'img_site.img_uploader',
